b8d70125ce71a42c88a52e9e356564047759a976
MINKS-JERRY <minkemj@gmail.com>
2025-05-21 10:44:09 +0200

    Remove admin

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,19 +1,8 @@
 import { Routes } from '@angular/router';
 import { AuthGuard as authGuard } from './guards/auth.guard';
-import { AdminComponent } from './components/admin/admin.component';
 
 export const routes: Routes = [
-  { path: 'admin', component: AdminComponent, canActivate: [authGuard] },
-  {
-    path: '',
-    redirectTo: '/tasks',
-    pathMatch: 'full'
-  },
-  {
-    path: 'tasks',
-    loadComponent: () => import('./components/task-list/task-list.component').then(m => m.TaskListComponent),
-    canActivate: [authGuard]
-  },
+  // Public routes
   {
     path: 'login',
     loadComponent: () => import('./components/login/login.component').then(m => m.LoginComponent)
@@ -21,5 +10,25 @@ export const routes: Routes = [
   {
     path: 'register',
     loadComponent: () => import('./components/register/register.component').then(m => m.RegisterComponent)
+  },
+
+  // Protected routes
+  {
+    path: 'tasks',
+    loadComponent: () => import('./components/task-list/task-list.component').then(m => m.TaskListComponent),
+    canActivate: [authGuard]
+  },
+
+  // Default route
+  {
+    path: '',
+    redirectTo: '/tasks',
+    pathMatch: 'full'
+  },
+
+  // Catch-all route
+  {
+    path: '**',
+    redirectTo: '/tasks'
   }
 ];
